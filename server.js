e8ec0a367205f0afcf610132095dfd02fb4238b5
Matt Stalzer <mjs58181@uga.edu>
2020-10-11 20:37:26 -0400

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true },{ useUnifiedTopology: true });
-mongoose.connect("mongodb://localhost/workout", { useNewUrlParser: true },{ useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout" {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 mongoose.set('debug', true);
 
 var db = mongoose.connection;
